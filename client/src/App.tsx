55ca585ea89443f992fb51992dea021d71c9c5ad
Archit <architmishra016@gmail.com>
2026-01-02 16:37:37 +0530

    Created Repo

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <h1>Let's start Doston !! Har Har Mahadev !</h1>
+    </>
+  )
+}
+
+export default App
---
+++ server/src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+export default app;
